30cbce74afb0b8683c0d2baa8a8838b27a1029eb
Victor Arana <victor@codersfree.com>
2025-03-01 22:10:03 -0500

    Introducción a Eloquent

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/prueba', function(){
+
+    $data = [
+        'body' => 'Prueba de tarea',
+        'user_id' => 1
+    ];
+
+
+    /* $task = new Task();
+
+    $task->body = 'Prueba de tarea';
+    $task->user_id = 1; 
+    $task->save();*/
+
+    $task = Task::create($data);
+
+    return $task;
+
 });
