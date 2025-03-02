483cbeecbfc8cdd3cc032b48ad3d07b1b2257932
Victor Arana <victor@codersfree.com>
2025-03-01 21:36:55 -0500

    Verbos HTTP

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,38 @@ Route::get('/', function () {
         'message' => 'Hola desde la API de Laravel',
     ]);
 });
+
+//Listar registros
+Route::get('users', function(){
+    return response()->json([
+        'message' => 'Listado de usuarios',
+    ]);
+});
+
+//Crear registros
+Route::post('users', function(){
+    return response()->json([
+        'message' => 'Crear usuario',
+    ]);
+});
+
+//Recuperar registros
+Route::get('users/{id}', function($id){
+    return response()->json([
+        'message' => 'Recuperar usuario con id ' . $id,
+    ]);
+});
+
+//Actualizar registros
+Route::put('users/{id}', function($id){
+    return response()->json([
+        'message' => 'Actualizar usuario con id ' . $id,
+    ]);
+});
+
+//Eliminar registros
+Route::delete('users/{id}', function($id){
+    return response()->json([
+        'message' => 'Eliminar usuario con id ' . $id,
+    ]);
+});
